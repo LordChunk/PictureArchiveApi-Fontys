d6303eacae675a6da16cdeb2745f32090714d4d4
Job <contact@fontys.me>
2018-12-03 17:35:42 +0100

    Fixed dependencies

--- DAL/User.cs
+++ DAL/User.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Models.User;
 
