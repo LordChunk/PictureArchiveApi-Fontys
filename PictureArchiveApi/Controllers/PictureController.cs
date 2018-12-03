41c5c9bcf9003e367977450526bf0d87f1fd12bb
Job <contact@chu.mk>
2018-12-03 16:24:19 +0100

    Can currently store images in temp folder in wwwroot

---
+++ PictureArchiveApi/Controllers/PictureController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace Logic.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PictureController : ControllerBase
+    {
+        private readonly IHostingEnvironment _environment;
+
+        public PictureController(IHostingEnvironment IHostingEnvironment)
+        {
+            _environment = IHostingEnvironment;
+        }
+
+        [HttpPost]
+        public async Task<object> Upload(string name)
+        {
+            var newFileName = string.Empty;
+
+            if (HttpContext.Request.Form.Files != null)
+            {
+                var fileName = string.Empty;
+                string PathDB = string.Empty;
+
+                var files = HttpContext.Request.Form.Files;
+
+                foreach (var file in files)
+                {
+                    if (file.Length > 0)
+                    {
+                        //Getting FileName
+                        fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.ToString();
+
+                        //Assigning Unique Filename (Guid)
+                        var myUniqueFileName = Convert.ToString(Guid.NewGuid());
+
+                        //Getting file Extension
+                        var FileExtension = Path.GetExtension(fileName);
+
+                        // concating  FileName + FileExtension
+                        newFileName = myUniqueFileName + FileExtension;
+
+                        // Combines two strings into a path.
+                        fileName = Path.Combine(_environment.WebRootPath, "demoImages") + $@"\{newFileName}";
+
+                        // if you want to store path of folder in database
+                        PathDB = "demoImages/" + newFileName;
+
+                        using (FileStream fs = System.IO.File.Create(fileName))
+                        {
+                            file.CopyTo(fs);
+                            fs.Flush();
+                        }
+                    }
+                }
+            }
+
+            return Ok();
+        }
+    }
+}
