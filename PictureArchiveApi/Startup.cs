e359b2704fcfada6a2a0eb84184d9aafbba32641
Job <contact@fontys.me>
2018-12-04 22:30:56 +0100

    Downgraded back to DotNet 2.1 because Azure doesn't support DotNet 2.2 yet

--- PictureArchiveApi/Startup.cs
+++ PictureArchiveApi/Startup.cs
@@ -85,7 +85,7 @@ namespace PictureArchiveApi
                     };
                 });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
